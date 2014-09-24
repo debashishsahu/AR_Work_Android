4a57cc254500adfe309dd1ed99880167b0ab5a06
Debashish Sahu <debashishsahu@outlook.com>
2014-09-24 17:58:01 +0530

    ARApplicationTemplate | Added Camera
    Creating a basic AR Template for use in AR Application | Added Camera
    
    TODO: Add Accelerometer support.

---
+++ ARApplicationTemplate/app/src/main/java/com/example/debashish/arapplicationtemplate/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.debashish.arapplicationtemplate;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+
+public class MainActivity extends Activity {
+
+    SurfaceView cameraPreview;
+    SurfaceHolder previewHolder;
+    Camera camera;
+    boolean inPreview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initialize and instantiate the variables
+        inPreview = false;
+
+        cameraPreview = (SurfaceView) findViewById(R.id.cameraPreview);
+        previewHolder = cameraPreview.getHolder();
+        previewHolder.addCallback(surfaceCallback);
+        previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        camera = Camera.open();
+    }
+
+    @Override
+    protected void onPause() {
+        if(inPreview){
+            camera.stopPreview();
+        }
+
+        camera.release();
+        camera = null;
+        inPreview = false;
+
+        super.onPause();
+    }
+
+    private Camera.Size getBestPreviewSize(int width, int height, Camera.Parameters parameters){
+        Camera.Size result = null;
+
+        for(Camera.Size size : parameters.getSupportedPreviewSizes()){
+            if(size.width <= width && size.height <= height){
+                if(result == null){
+                    result = size;
+                } else{
+                    int resultArea = result.width * result.height;
+                    int newArea = size.width * size.height;
+
+                    if(newArea > resultArea){
+                        result = size;
+                    }
+                }
+            }
+        }
+
+        return (result);
+    }
+
+    SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                camera.setPreviewDisplay(previewHolder);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            Camera.Parameters parameters = camera.getParameters();
+            Camera.Size size = getBestPreviewSize(width, height, parameters);
+
+            if(size != null){
+                parameters.setPreviewSize(size.width, size.height);
+                camera.setParameters(parameters);
+                camera.startPreview();
+                inPreview = true;
+            }
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // not used
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
