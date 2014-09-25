851c324b668101a89e7bf3a0d143e6bb2d897166
Debashish Sahu <debashishsahu@outlook.com>
2014-09-25 17:58:23 +0530

    AR with Overlay
    Project to display overlay on the screen with the sensor data.

---
+++ ARWidgetOverlays/app/src/main/java/com/example/debashish/arwidgetoverlays/MainActivity.java
@@ -0,0 +1,273 @@
+package com.example.debashish.arwidgetoverlays;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+
+public class MainActivity extends Activity {
+
+    // variables declared for using the camera
+    SurfaceView cameraPreview;
+    SurfaceHolder previewHolder;
+    Camera camera;
+    boolean inPreview;
+
+    // variables declared for orientation sensor
+    final static String TAG = "PAAR";
+    SensorManager sensorManager;
+
+    int orientationSensor;
+    float headingAngle;
+    float pitchAngle;
+    float rollAngle;
+
+    // variables declared for accelerometer
+    int accelerometerSensor;
+    float xAxis;
+    float yAxis;
+    float zAxis;
+
+    // variables declared for GPS
+    LocationManager locationManager;
+    double latitude;
+    double longitude;
+    double altitude;
+
+    // variables for the TextViews
+    TextView xAxisValue;
+    TextView yAxisValue;
+    TextView zAxisValue;
+    TextView headingValue;
+    TextView pitchValue;
+    TextView rollValue;
+    TextView altitudeValue;
+    TextView latitudeValue;
+    TextView longitudeValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initialize and instantiate the variables for camera
+        inPreview = false;
+
+        cameraPreview = (SurfaceView) findViewById(R.id.cameraPreview);
+        previewHolder = cameraPreview.getHolder();
+        previewHolder.addCallback(surfaceCallback);
+        previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        // initialize and instantiate the variables for orientation sensor
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        orientationSensor = Sensor.TYPE_ORIENTATION;
+        sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(orientationSensor), SensorManager.SENSOR_DELAY_NORMAL);
+
+        // initialize and instantiate the variables for accelerometer sensor
+        accelerometerSensor = Sensor.TYPE_ACCELEROMETER;
+        sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(accelerometerSensor), SensorManager.SENSOR_DELAY_NORMAL);
+
+        // initialize and instantiate the variables for GPS sensor
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 2, locationListener);
+
+        // getting access to the TextViews on the layout
+        xAxisValue = (TextView) findViewById(R.id.xAxisValue);
+        yAxisValue = (TextView) findViewById(R.id.yAxisValue);
+        zAxisValue = (TextView) findViewById(R.id.zAxisValue);
+        headingValue = (TextView) findViewById(R.id.headingValue);
+        pitchValue = (TextView) findViewById(R.id.pitchValue);
+        rollValue = (TextView) findViewById(R.id.rollValue);
+        altitudeValue = (TextView) findViewById(R.id.altitudeValue);
+        latitudeValue = (TextView) findViewById(R.id.latitudeValue);
+        longitudeValue = (TextView) findViewById(R.id.longitudeValue);
+    }
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+            altitude = location.getAltitude();
+
+            Log.d(TAG, "Latitude: " + String.valueOf(latitude));
+            Log.d(TAG, "Longitude: " + String.valueOf(longitude));
+            Log.d(TAG, "Altitude: " + String.valueOf(altitude));
+
+            // display values in the corresponding TextViews
+            latitudeValue.setText(String.valueOf(latitude));
+            longitudeValue.setText(String.valueOf(longitude));
+            altitudeValue.setText(String.valueOf(altitude));
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // not used
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // not used
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // not used
+        }
+    };
+
+    final SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if(event.sensor.getType() == Sensor.TYPE_ORIENTATION){
+                headingAngle = event.values[0];
+                pitchAngle = event.values[1];
+                rollAngle = event.values[2];
+
+                Log.d(TAG, "Heading: " + String.valueOf(headingAngle));
+                Log.d(TAG, "Pitch: " + String.valueOf(pitchAngle));
+                Log.d(TAG, "Roll: " + String.valueOf(rollAngle));
+
+                // display values in the corresponding TextViews
+                headingValue.setText(String.valueOf(headingAngle));
+                pitchValue.setText(String.valueOf(pitchAngle));
+                rollValue.setText(String.valueOf(rollAngle));
+
+            } else if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                xAxis = event.values[0];
+                yAxis = event.values[1];
+                zAxis = event.values[2];
+
+                Log.d(TAG, "X Axis: " + String.valueOf(xAxis));
+                Log.d(TAG, "Y Axis: " + String.valueOf(yAxis));
+                Log.d(TAG, "Z Axis: " + String.valueOf(zAxis));
+
+                // display values in the corresponding TextViews
+                xAxisValue.setText(String.valueOf(xAxis));
+                yAxisValue.setText(String.valueOf(yAxis));
+                zAxisValue.setText(String.valueOf(zAxis));
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // not used
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        camera = Camera.open();
+
+        sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(orientationSensor), SensorManager.SENSOR_DELAY_NORMAL);
+
+        sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(accelerometerSensor), SensorManager.SENSOR_DELAY_NORMAL);
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 2, locationListener);
+    }
+
+    @Override
+    protected void onPause() {
+        if(inPreview){
+            camera.stopPreview();
+        }
+
+        camera.release();
+        camera = null;
+        inPreview = false;
+
+        locationManager.removeUpdates(locationListener);
+        sensorManager.unregisterListener(sensorEventListener);
+
+        super.onPause();
+    }
+
+    private Camera.Size getBestPreviewSize(int width, int height, Camera.Parameters parameters){
+        Camera.Size result = null;
+
+        for(Camera.Size size : parameters.getSupportedPreviewSizes()){
+            if(size.width <= width && size.height <= height){
+                if(result == null){
+                    result = size;
+                } else{
+                    int resultArea = result.width * result.height;
+                    int newArea = size.width * size.height;
+
+                    if(newArea > resultArea){
+                        result = size;
+                    }
+                }
+            }
+        }
+
+        return (result);
+    }
+
+    SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                camera.setPreviewDisplay(previewHolder);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            Camera.Parameters parameters = camera.getParameters();
+            Camera.Size size = getBestPreviewSize(width, height, parameters);
+
+            if(size != null){
+                parameters.setPreviewSize(size.width, size.height);
+                camera.setParameters(parameters);
+                camera.startPreview();
+                inPreview = true;
+            }
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // not used
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
